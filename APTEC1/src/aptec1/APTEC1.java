de9dd81c6e842559f1ddfa2ede75fdd83c4aef83
Jimena <jimenas27@gmail.com>
2019-11-14 14:28:33 -0600

    comentario

---
+++ APTEC1/src/aptec1/APTEC1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aptec1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class APTEC1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Hola 1234
+        //Hola 5678
+    }
+    
+}
