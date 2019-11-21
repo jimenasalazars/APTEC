429ae8662c132cea6752fa361b495048f5322419
SamuelMongeLeon <samuel.monge02@gmail.com>
2019-11-20 18:33:17 -0600

    graph finish

--- APTEC1/src/Logic/Graph.java
+++ APTEC1/src/Logic/Graph.java
@@ -7,7 +7,7 @@ package Logic;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor.uijyhtgrfed
  */
 
 import java.awt.Graphics2D;
@@ -132,6 +132,7 @@ public class Graph {
                 return i;
             }
         }
+        
         return -1;
     }
     
